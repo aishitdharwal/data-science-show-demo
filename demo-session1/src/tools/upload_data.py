9b07e90df36fc23af9dc8cfda90d50c96b119363
mitulds <mitulppatel8@gmail.com>
2022-12-22 14:00:48 -0700

    update script to upload all data to db

---
+++ demo-session1/src/tools/upload_data.py
@@ -0,0 +1,70 @@
+import os
+import argparse
+from pathlib import Path
+
+import mysql.connector as msql
+from dotenv import load_dotenv
+from mysql.connector import Error
+
+from utils import get_data, create_db_schema
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--input_data", help="provide path to input data", required=True, type=str)
+parser.add_argument("-db", "--create_db", help="create db or not", default=False, type=bool)
+parser.add_argument("-t", "--table_name", help="provide table name", type=str)
+args = parser.parse_args()
+
+# define variables
+input_data = Path(args.input_data)
+table_name = args.table_name
+
+df = get_data(input_data)
+col_type, values = create_db_schema(df)
+
+
+# read mysql password
+env_path = Path("./.env")
+load_dotenv(dotenv_path=env_path)
+mysql_pass = os.getenv("MYSQL_PASS")
+
+# create a database
+if args.create_db:
+    try:
+        conn = msql.connect(host='localhost',
+                            user='root',
+                            password=mysql_pass)
+        if conn.is_connected():
+            cursor = conn.cursor()
+            cursor.execute('DROP DATABASE IF EXISTS groceries')
+            cursor.execute('CREATE DATABASE groceries')
+            print("Database is created")
+            cursor.execute('SHOW DATABASES')
+            record = cursor.fetchone()
+            print("You're connected to database: ", record)
+            #cursor.close()
+            #conn.close()
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+else:
+    try:
+        conn = msql.connect(host='localhost',
+                            user='root',
+                            password=mysql_pass)
+        if conn.is_connected():
+            cursor = conn.cursor()
+            cursor.execute('USE groceries')
+            cursor.execute(f'DROP TABLE IF EXISTS {table_name};')
+            print(f"Creating table {table_name}....")
+            cursor.execute(f'CREATE TABLE {table_name}({col_type})')
+            print("Table is created....")
+            
+            #loop through the data frame
+            for i,row in df.iterrows():
+                #here %S means string values
+                sql = f'INSERT INTO groceries.{table_name} VALUES ({values})'
+                cursor.execute(sql, tuple(row))
+                # the connection is not auto committed by default, so we must commit to save our changes
+                conn.commit()
+            print("Record inserted")
+    except Error as e:
+        print("Error while connecting to MySQL", e)
