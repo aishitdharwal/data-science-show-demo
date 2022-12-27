1a9aab5cae4b41672f52f7a129be5bf299341eff
mitulds <mitulppatel8@gmail.com>
2022-12-27 04:04:05 -0700

    implement config file

--- demo-session1/src/tools/upload_data.py
+++ demo-session1/src/tools/upload_data.py
@@ -5,13 +5,17 @@ from pathlib import Path
 import mysql.connector as msql
 from dotenv import load_dotenv
 from mysql.connector import Error
-from utils import create_db_schema, get_data
+from utils import create_db_schema, get_data, load_yaml, get_path
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--input_data", help="provide path to input data", type=str)
+#parser.add_argument("-d", "--input_data", help="provide path to input data", type=str)
 parser.add_argument("-db", "--create_db", help="create db or not", default=False, type=bool)
-parser.add_argument("-t", "--table_name", help="provide table name", type=str)
+#parser.add_argument("-t", "--table_name", help="provide table name", type=str)
+
+parser.add_argument("-t", "--task", help="this will point to a task \
+                    location into the config.yaml file.", type=str)
 args = parser.parse_args()
+
 
 # read mysql password
 env_path = Path("./.env")
@@ -39,8 +43,19 @@ if args.create_db:
 # create a table in the database
 else:
     # define variables
-    input_data = Path(args.input_data)
-    table_name = args.table_name
+    # NEW ADDITION TO CODE -----------------------------------
+    config = load_yaml("/Users/mits/Desktop/coursera/weekly_projects/data-science-show-demo/demo-session1/config/config.yaml")
+    args_task = 'csv-to-database'
+    config_import = config[args.task]["import"]
+    config_import[0]["import"]
+
+    input_data = get_path(config_import[0]["import"]["dirpath"], 
+            config_import[0]["import"]["prefix_filename"]+ '.' +
+            config_import[0]["import"]["file_extension"])
+    table_name = os.path.basename(input_data).split('.')[0]
+    #----------------------------------------------------------------------
+    #input_data = Path(args.input_data)
+    #table_name = args.table_name
 
     # get data and create db schema
     df = get_data(input_data)
